820eb166ca7d95428e6b80f3cf7c2cc2007588bf
Pranjal Kumar <pranjal.kumar.153@gmail.com>
2020-11-20 15:51:46 +0530

    Added the functionality of spend button.

--- budget_manager/popup.js
+++ budget_manager/popup.js
@@ -1,12 +1,32 @@
 $(function() {
     chrome.storage.sync.get(["total", "limit"], function(budget) {
         if (budget.total) {
-            var total_amount = parseInt(budget.total);
-            $("#total_spent_amount").text(toString(total_amount));
+            var total_amount = parseInt(budget.total, 10);
+            $("#total_spent_amount").text(total_amount);
+        } else if (!(budget.total)) {
+            $("#total_spent_amount").text("NA");
         }
         if (budget.limit) {
-            var limit_value = parseInt(budget.limit);
-            $("#spending_limit").text(toString(limit_value));
+            var limit_value = parseInt(budget.limit, 10);
+            $("#spending_limit").text(limit_value);
+        } else if (!(budget.limit)) {
+            $("#spending_limit").text("NA");
         }
+    });
+    $("#spend_amount").click(function() {
+        var newTotal = 0;
+        if ($("#entered_amount").val()) {
+            var spent_value = parseInt($("#entered_amount").val(), 10);
+            newTotal += spent_value;
+            $("#entered_amount").val("");
+        }
+        chrome.storage.sync.get("total", function(budget) {
+            if (budget.total)
+                newTotal += parseInt(budget.total, 10);
+            chrome.storage.sync.set({ "total": newTotal }, function() {
+                $("#total_spent_amount").text(newTotal);
+                $("#entered_amount").val("");
+            });
+        });
     });
 });
