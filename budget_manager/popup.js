5ac56c9027e75d094df385109e08a40caf69ae74
Pranjal Kumar <pranjal.kumar.153@gmail.com>
2020-11-20 16:47:53 +0530

    Added the limit-functionality.

--- budget_manager/popup.js
+++ budget_manager/popup.js
@@ -29,4 +29,13 @@ $(function() {
             });
         });
     });
+    $("#set_limit").click(function() {
+        if ($("#set_the_limit").val()) {
+            var limit_value = parseInt($("#set_the_limit").val());
+            chrome.storage.sync.set({ "limit": limit_value }, function() {
+                $("#spending_limit").text(limit_value);
+                $("#set_the_limit").val("");
+            });
+        }
+    });
 });
