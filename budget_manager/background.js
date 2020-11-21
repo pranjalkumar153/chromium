fbbfadcdb025866def2b650b0362bdf1a7ad7041
Pranjal Kumar <pranjal.kumar.153@gmail.com>
2020-11-21 21:20:06 +0530

    minor corrections.

--- budget_manager/background.js
+++ budget_manager/background.js
@@ -1,5 +1,5 @@
 var contextMenuObject = {
-    "id": "spending_money",
+    "id": "SpendMoney",
     "title": "Spend Money",
     "contexts": ["selection"]
 };
@@ -7,31 +7,32 @@ var contextMenuObject = {
 chrome.contextMenus.create(contextMenuObject);
 
 function isInt(value) {
-    return !isNaN(value) && parseInt(value) == value;
+    return !isNaN(value) && parseInt(value) == value && !isNaN(parseInt(value, 10));
 }
 
 chrome.contextMenus.onClicked.addListener(function(clickData) {
-    if (clickData.menuItemId == "spending_money" && clickData.selectionText) {
+    if (clickData.menuItemId == "SpendMoney" && clickData.selectionText) {
         if (isInt(clickData.selectionText)) {
-            var newTotal = 0;
-            chrome.storage.sync.get("total", function(budget) {
+            chrome.storage.sync.get(["limit", "total"], function(budget) {
+                var newTotal = 0;
                 if (budget.total) {
                     newTotal += parseInt(budget.total);
                 }
+                newTotal += parseInt(clickData.selectionText);
+                chrome.storage.sync.set({ "total": newTotal }, function() {
+                    var notificationObject = {
+                        type: "basic",
+                        title: "Limit Exceeded!!",
+                        message: "Looks like you have spent more than your sepnding limit!!",
+                        iconUrl: "moneyicon.jpg"
+                    }
+                    if (parseInt(budget.limit) <= parseInt(budget.total)) {
+                        chrome.notifications.create(notificationObject, function() {
+                            console.log("Notified!!");
+                        });
+                    }
+                });
             });
-            newTotal += parseInt(clickData.selectionText);
-            chrome.storage.sync.set({ "total": newTotal });
         }
     }
-    chrome.storage.sync.get(["total", "limit"], function(budget) {
-        var notify = {
-            type: "basic",
-            iconUrl: "icon.jpg",
-            title: "Limit Exceeded",
-            message: "Seems like you have spent more than your spending limit!!"
-        };
-        if (parseInt(budget.total) >= parseInt(budget.limit)) {
-            chrome.notifications.create(notify);
-        }
-    });
 });
