b829ec87e36b0754cf301b3100d31688c65945e9
Pranjal Kumar <pranjal.kumar.153@gmail.com>
2020-11-23 02:10:54 +0530

    Testing the functionality of background.js

--- budget_manager/background.js
+++ budget_manager/background.js
@@ -1,5 +1,5 @@
 var contextMenuObject = {
-    "id": "SpendBucks",
+    "id": "SpendNow",
     "title": "Spend Money",
     "contexts": ["selection"]
 };
@@ -12,13 +12,13 @@ function isInt(value) {
 
 
 chrome.contextMenus.onClicked.addListener(function(clickdata) {
-    if (clickdata.menuItemId == "spendBucks" && clickdata.selectionText) {
+    if (clickdata.menuItemId == "spendNow" && clickdata.selectionText) {
         var newTotal = 0; //issue resolved
         var d = new Date(); //issue resolved
         var spent_now = ""; //issue resolved
         var total_till_now = 0; //issue resolved
         var page_url = ""; //issue resolved
-        if (parseInt(clickdata.selectionText) && isInt(clickdata.selectionText)) {
+        if (isInt(clickdata.selectionText)) {
             chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
                 page_url = tabs[0].url;
                 console.log(page_url);
@@ -44,24 +44,8 @@ chrome.contextMenus.onClicked.addListener(function(clickdata) {
                             x = [spent_obj_information];
                         }
                         console.log(x);
-                        chrome.storage.sync.set({ "spent_data_array": x }, function() {
-                            chrome.storage.sync.set({ "total": newTotal }, function() {
-                                chrome.storage.sync.get(["total", "limit"], function(budget) {
-                                    if (budget.total || parseInt(budget.total) == 0) {
-                                        var total_amount = parseInt(budget.total, 10);
-                                        $("#total_spent_amount").text(total_amount);
-                                    } else if (!(budget.total)) {
-                                        $("#total_spent_amount").text("NA");
-                                    }
-                                    if (budget.limit) {
-                                        var limit_value = parseInt(budget.limit, 10);
-                                        $("#spending_limit").text(limit_value);
-                                    } else if (!(budget.limit)) {
-                                        $("#spending_limit").text("NA");
-                                    }
-                                });
-                            });
-                        });
+                        chrome.storage.sync.set({ "spent_data_array": x });
+                        chrome.storage.sync.set({ "total": newTotal });
                     });
                 });
             });
