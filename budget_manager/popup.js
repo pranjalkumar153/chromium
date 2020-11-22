666a716a43e30e135a56467fa6493105047df833
Pranjal Kumar <pranjal.kumar.153@gmail.com>
2020-11-22 23:51:10 +0530

    Corrections required in background.js

--- budget_manager/popup.js
+++ budget_manager/popup.js
@@ -13,66 +13,6 @@ $(function() {
             $("#spending_limit").text("NA");
         }
     });
-    // $("#spend_amount").click(function() {
-    //     var newTotal = 0;
-    //     var d = new Date();
-    //     var spent_now = "";
-    //     var total_till_now = 0;
-    //     var page_url = "";
-    //     if ($("#entered_amount").val()) {
-    //         var spent_value = parseInt($("#entered_amount").val(), 10);
-    //         spent_now = spent_value;
-    //         newTotal += spent_value;
-    //         $("#entered_amount").val("");
-    //     }
-    //     chrome.storage.sync.get("total", function(budget) {
-    //         if (budget.total)
-    //             newTotal += parseInt(budget.total, 10);
-    //         chrome.storage.sync.set({ "total": newTotal }, function() {
-    //             $("#total_spent_amount").text(newTotal);
-    //             $("#entered_amount").val("");
-    //             total_till_now = newTotal;
-    //         });
-    //         chrome.storage.sync.get(["total", "limit"], function(budget) {
-    //             var notify = {
-    //                 type: "basic",
-    //                 title: "Limit Exceeded",
-    //                 message: "Seems like you have spent more than your spending limit!!",
-    //                 iconUrl: "icon.jpg"
-    //             };
-    //             if (parseInt(budget.total) >= parseInt(budget.limit)) {
-    //                 chrome.notifications.create(notify);
-    //             }
-    //         });
-    //     });
-    //     // will make an array of this type of object which will fetch all the requirements
-    //     // for the graph plotting.
-    //     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    //         page_url = tabs[0].url;
-    //         console.log(page_url); //works properly
-    //         var spent_obj_information = {
-    //             date: d,
-    //             spent_amount: spent_now,
-    //             total: total_till_now,
-    //             url_of_website: page_url
-    //         };
-    //         console.log(spent_obj_information);
-    //         var spent_data_array = [];
-    //         // storing the array to chrome store
-    //         chrome.storage.sync.get("spent_data_array", function(budget) {
-    //             if (budget.spent_data_array) {
-    //                 spent_data_array = budget.spent_data_array;
-    //                 spent_data_array.push(spent_obj_information);
-    //                 chrome.storage.sync.set({ "spent_data_array": spent_data_array });
-    //                 console.log(spent_data_array);
-    //             } else {
-    //                 spent_data_array = [spent_obj_information];
-    //                 chrome.storage.sync.set({ "spent_data_array": spent_data_array });
-    //                 console.log(spent_data_array);
-    //             }
-    //         });
-    //     });
-    // });
     $("#spend_amount").click(function() {
         var newTotal = 0; //sorted
         var d = new Date(); //sorted
