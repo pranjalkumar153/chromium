55436a0db5cfe7757ffd74f8c046a015ee79f607
Pranjal Kumar <pranjal.kumar.153@gmail.com>
2020-11-29 20:01:07 +0530

    Error related data resolved to some extent.

--- budget_manager/background.js
+++ budget_manager/background.js
@@ -62,39 +62,74 @@ chrome.contextMenus.onClicked.addListener(function(clickData) {
                             x = [spent_obj_information];
                         }
                         console.log(x);
-                        chrome.storage.sync.set({ "spent_data_array": x });
+                        chrome.storage.sync.set({ "spent_data_array": x }, function() {
+                            var obj = x[x.length - 1];
+                            if (RegExp("/www.flipkart.com/").test(obj.url_of_website)) {
+                                chrome.storage.sync.get("flipkart", function(budget) {
+                                    var x;
+                                    if (budget.flipkart) {
+                                        x = budget.fipkart;
+                                        x.push(obj);
+                                    } else {
+                                        x = [obj];
+                                    }
+                                    chrome.storage.sync.set({ "flipkart": x });
+                                    console.log(x);
+                                });
+                            } else if (RegExp("/www.amazon.in/").test(obj.url_of_website)) {
+                                chrome.storage.sync.get("amazon", function(budget) {
+                                    var x;
+                                    if (budget.amazon) {
+                                        x = budget.amazon;
+                                        x.push(obj);
+                                    } else {
+                                        x = [obj];
+                                    }
+                                    chrome.storage.sync.set({ "amazon": x });
+                                    console.log(x);
+                                });
+                            } else if (RegExp("/www.myntra.com/").test(obj.url_of_website)) {
+                                chrome.storage.sync.get("myntra", function(budget) {
+                                    var x;
+                                    if (budget.myntra) {
+                                        x = budget.myntra;
+                                        x.push(obj);
+                                    } else {
+                                        x = [obj];
+                                    }
+                                    chrome.storage.sync.set({ "myntra": x });
+                                    console.log(x);
+                                });
+                            } else if (RegExp("/www.snapdeal.com/").test(obj.url_of_website)) {
+                                chrome.storage.sync.get("snapdeal", function(budget) {
+                                    var x;
+                                    if (budget.snapdeal) {
+                                        x = budget.snapdeal;
+                                        x.push(obj);
+                                    } else {
+                                        x = [obj];
+                                    }
+                                    chrome.storage.sync.set({ "snapdeal": x });
+                                    console.log(x);
+                                });
+                            } else {
+                                chrome.storage.sync.get("others", function(budget) {
+                                    var x;
+                                    if (budget.others) {
+                                        x = budget.others;
+                                        x.push(obj);
+                                    } else {
+                                        x = [obj];
+                                    }
+                                    chrome.storage.sync.set({ "others": x });
+                                    console.log(x);
+                                });
+                            }
+                        });
                     });
                 });
             });
         }
-        var flipkart = [];
-        var Amazon = [];
-        var Myntra = [];
-        var Snapdeal = [];
-        var others = [];
-
-        chrome.storage.sync.get("spent_data_array", function(budget) {
-            var x = budget.spent_data_array;
-            if (x) {
-                for (var i = 0; i < x.length; i++) {
-                    if (RegExp("/www.flipkart.com/").test(x[i].url_of_website)) {
-                        flipkart.push(x[i]);
-                    } else if (RegExp("/www.amazon.in/").test(x[i].url_of_website)) {
-                        Amazon.push(x[i]);
-                    } else if (RegExp("/www.mytra.com/").test(x[i].url_of_website)) {
-                        Myntra.push(x[i]);
-                    } else if (RegExp("/www.snapdeal.com/").test(x[i].url_of_website)) {
-                        Snapdeal.push(x[i]);
-                    } else others.push(x[i]);
-                }
-                chrome.storage.sync.set({ "flipkart": flipkart });
-                chrome.storage.sync.set({ "amazon": Amazon });
-                chrome.storage.sync.set({ "myntra": Myntra });
-                chrome.storage.sync.set({ "snapdeal": Snapdeal });
-                chrome.storage.sync.set({ "others": others });
-            }
-
-        });
     }
 });
 
@@ -104,114 +139,3 @@ chrome.contextMenus.onClicked.addListener(function(clickData) {
 // 3. Myntra
 // 4. Snapdeal
 // 5. Others
-
-// Knowledge of regex expression is required.
-// And file handling with javascript
-
-
-//separating out data for Flipkart.
-var flipkart = [];
-var Amazon = [];
-var Myntra = [];
-var Snapdeal = [];
-var others = [];
-
-chrome.storage.sync.get("spent_data_array", function(budget) {
-    var x = budget.spent_data_array;
-    if (x) {
-        for (var i = 0; i < x.length; i++) {
-            if (RegExp("/www.flipkart.com/").test(x[i].url_of_website)) {
-                flipkart.push(x[i]);
-            } else if (RegExp("/www.amazon.in/").test(x[i].url_of_website)) {
-                Amazon.push(x[i]);
-            } else if (RegExp("/www.mytra.com/").test(x[i].url_of_website)) {
-                Myntra.push(x[i]);
-            } else if (RegExp("/www.snapdeal.com/").test(x[i].url_of_website)) {
-                Snapdeal.push(x[i]);
-            } else others.push(x[i]);
-        }
-        chrome.storage.sync.set({ "flipkart": flipkart });
-        chrome.storage.sync.set({ "amazon": Amazon });
-        chrome.storage.sync.set({ "myntra": Myntra });
-        chrome.storage.sync.set({ "snapdeal": Snapdeal });
-        chrome.storage.sync.set({ "others": others });
-    }
-
-});
-// Learn file handling in Javascript to continue further.
-// Larn chrome API fileSystem.
-
-// chrome.storage.sync.get("flipkart", function(budget) {
-//     console.log(budget.amazon);
-// });
-
-function resolve_for_graph(arr) {
-    var arr_new = [];
-    if (!arr) {
-        return [];
-    } else {
-        arr_new.push(arr[0]);
-        for (var i = 1; i < arr.length; i++) {
-            if (arr_new[arr_new.length - 1].date.dd == arr[i].date.dd &&
-                arr_new[arr_new.length - 1].date.mm == arr[i].date.mm &&
-                arr_new[arr_new.length - 1].date.yy == arr[i].date.yy) {
-                arr_new[arr_new.length - 1].spent_amount += arr[i].spent_amount;
-            } else {
-                arr_new.push(arr[i]);
-            }
-        }
-        return arr_new;
-    }
-}
-// spent_data_array: spent_data_array_graph
-// flipkart: flipkart_graph
-// amazon: amazon_graph
-// myntra: amazon_graph
-// snapdeal: snapdeal_graph
-// others: others_graph
-// overall spendings
-chrome.storage.sync.get("spent_data_array", function(budget) {
-    console.log("Spent_data_array");
-    console.log(budget.spent_data_array);
-    var x = resolve_for_graph(budget.spent_data_array);
-    console.log("after resolving");
-    console.log(x);
-    chrome.storage.sync.set({ "spent_data_array_graph": x });
-});
-// flipkart
-chrome.storage.sync.get("flipkart", function(budget) {
-    var x = resolve_for_graph(budget.flipkart);
-    console.log(x);
-    chrome.storage.sync.set({ "flipkart_graph": x });
-});
-// amazon
-chrome.storage.sync.get("amazon", function(budget) {
-    var x = resolve_for_graph(budget.amazon);
-    console.log(x);
-    chrome.storage.sync.set({ "amazon_graph": x });
-});
-
-// myntra
-chrome.storage.sync.get("myntra", function(budget) {
-    var x = resolve_for_graph(budget.myntra);
-    console.log(x);
-    chrome.storage.sync.set({ "myntra_graph": x });
-});
-
-// snapdeal
-chrome.storage.sync.get("snapdeal", function(budget) {
-    var x = resolve_for_graph(budget.snapdeal);
-    console.log(x);
-    chrome.storage.sync.set({ "snapdeal_graph": x });
-});
-
-// others
-chrome.storage.sync.get("others", function(budget) {
-    var x = resolve_for_graph(budget.others);
-    console.log(x);
-    chrome.storage.sync.set({ "others_graph": x });
-});
-
-// chrome.storage.sync.get("flipkart_graph", function(budget) {
-//     console.log(budget.flipkart);
-// });
