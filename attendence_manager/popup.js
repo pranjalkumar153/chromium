b25039f9e83e9086ccda154c58a2c23554adfb64
PRANJAL KUMAR <pranjal.kumar.153@gmail.com>
2021-01-04 13:16:25 +0530

    Same old rubbish

--- attendence_manager/popup.js
+++ attendence_manager/popup.js
@@ -6,21 +6,23 @@ $(function() {
         var sub_name = $("subject_name").val();
         chrome.storage.sync.get(["subject_list"], function(res) {
             var sub_list;
-            if (!res.subject_list) {
-                sub_list = [];
-                sub_list.push(sub_name);
-                chrome.storage.sync.set({ "subject_list": sub_list }, function() {
-                    console.log("New Subject Added!!");
-                    alert("New Subject Added Successfully!!");
-                });
-            } else {
-                sub_list = res.subject_list;
-                sub_list.push(sub_name);
-                chrome.storage.sync.set({ "subject_list": sub_list }, function() {
-                    console.log("New Subject Added!!");
-                    alert("New Subject Added Successfully!!");
-                });
+            if (sub_name != "" && sub_name) {
+                if (!res.subject_list) {
+                    sub_list = [];
+                    sub_list.push(sub_name);
+                    chrome.storage.sync.set({ "subject_list": sub_list }, function() {
+                        console.log("New Subject Added!!");
+                        alert("New Subject Added Successfully!!");
+                    });
+                } else {
+                    sub_list = res.subject_list;
+                    sub_list.push(sub_name);
+                    chrome.storage.sync.set({ "subject_list": sub_list }, function() {
+                        console.log("New Subject Added!!");
+                        alert("New Subject Added Successfully!!");
+                    });
 
+                }
             }
         });
     });
