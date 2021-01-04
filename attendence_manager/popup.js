431b9f7eece7d7e56b1443dfcc48c9e860377613
PRANJAL KUMAR <pranjal.kumar.153@gmail.com>
2021-01-04 13:06:40 +0530

    Added the add_subject button functionality

---
+++ attendence_manager/popup.js
@@ -0,0 +1,25 @@
+$(function() {
+    $("#add_subject").click(function() {
+        var sub_name = $("subject_name").val();
+        chrome.storage.sync.get(["subject_list"], function(res) {
+            var sub_list;
+            if (!res.subject_list) {
+                sub_list = [];
+                sub_list.push(sub_name);
+                chrome.storage.sync.set({ "subject_list": sub_list }, function() {
+                    console.log("New Subject Added!!");
+                    alert("New Subject Added Successfully!!");
+                });
+            } else {
+                sub_list = res.subject_list;
+                sub_list.push(sub_name);
+                chrome.storage.sync.set({ "subject_list": sub_list }, function() {
+                    console.log("New Subject Added!!");
+                    alert("New Subject Added Successfully!!");
+                });
+
+            }
+        });
+    });
+    $("#remove_subject").click(function() {});
+});
