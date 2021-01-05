5cb4072a521602773065ed7eda13d60af6d68333
PRANJAL KUMAR <pranjal.kumar.153@gmail.com>
2021-01-05 23:41:12 +0530

    will review again

--- attendence_manager/functionality.js
+++ attendence_manager/functionality.js
@@ -2,5 +2,8 @@ $(function() {
     //=====================================================================================//
     //FUNCTIONALITY FOR MARKING THE ATTENDENCE                                             //
     //=====================================================================================//
+    $(document).on("click", "button", function() {
+        console.log($(this).parent().parent().parent());
+    });
 
 });
